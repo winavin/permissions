ac6455ce093e9b37ff223eb17373e9ae6353df71
winavin <winavin@gmail.com>
2025-04-29 12:45:25 +0530

    Middlewares Added

---
+++ src/Middlewares/HasAnyPermision.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Winavin\Permissions\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class HasAnyPermision
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string|array $permissions, $team = null): Response
+    {
+        if(! Auth::user()?->hasAnyPermission($permissions, $team)) {
+            abort(403, 'You do not have the required permission(s) to access this resource.');
+        }
+        return $next($request);
+    }
+}
